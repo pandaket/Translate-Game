242e78fafce51c20ff80dfd07a371c5a54d98de6
GZNigmatzyanova <GZNig@outlook.com>
2016-05-23 22:39:27 +0300

    new design

--- ModelsGame/app/src/main/java/com/example/modelsgame/DoubleGameActivity.java
+++ ModelsGame/app/src/main/java/com/example/modelsgame/DoubleGameActivity.java
@@ -27,11 +27,7 @@ public class DoubleGameActivity extends AppCompatActivity {
     TextView name_player1;
     Button createServer_btn, createConnect_btn;
     View.OnClickListener listener;
-    private Button mButtonOpen  = null;
-    private Button mButtonSend  = null;
-    private Button mButtonClose = null;
-    private LaptopServer mServer = null;
-    private static final String LOG_TAG = "myServerApp";
+
 
     void Connect()
     {
@@ -50,93 +46,18 @@ public class DoubleGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_double_game);
 //------------------------Регистрируем доступность сети
-        mNetworkMonitor = new NetworkMonitor();
-        IntentFilter intentFilter = new IntentFilter();
-        //указываем, какие именно события надо получить
-        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
-        //регистрируем в системе
-        registerReceiver(mNetworkMonitor,intentFilter);
+//        mNetworkMonitor = new NetworkMonitor();
+//        IntentFilter intentFilter = new IntentFilter();
+//        //указываем, какие именно события надо получить
+//        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+//        //регистрируем в системе
+//        registerReceiver(mNetworkMonitor,intentFilter);
 //-------------------
         //создаем объекты кнопок из интерфейса
         createServer_btn = (Button) findViewById(R.id.btn_create_server);
         createConnect_btn = (Button) findViewById(R.id.btn_create_client);
 
 
-        /////////
-
-        mButtonOpen = (Button) findViewById(R.id.button_open_connection);
-        mButtonSend = (Button) findViewById(R.id.button_send_connection);
-        mButtonClose = (Button) findViewById(R.id.button_close_connection);
-        mButtonSend.setEnabled(false);
-        mButtonClose.setEnabled(false);
-
-        mButtonOpen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-        /* создаем объект для работы с сервером*/
-                mServer = new LaptopServer();
-        /* Открываем соединение. Открытие должно происходить в отдельном потоке от ui */
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            mServer.openConnection();
-                    /*
-                        устанавливаем активные кнопки для отправки данных
-                        и закрытия соедиения. Все данные по обновлению интерфеса должны
-                        обрабатывается в Ui потоке, а так как мы сейчас находимся в
-                        отдельном потоке, нам необходимо вызвать метод  runOnUiThread()
-                    */
-                            runOnUiThread(new Runnable() {
-
-                                @Override
-                                public void run() {
-                                    mButtonSend.setEnabled(true);
-                                    mButtonClose.setEnabled(true);
-                                }
-                            });
-                        } catch (Exception e) {
-                            Log.e(LOG_TAG, e.getMessage());
-                            mServer = null;
-                        }
-                    }
-                }).start();
-            }
-        });
-
-        mButtonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mServer == null) {
-                    Log.e(LOG_TAG, "Сервер не создан");
-                }
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                    /* отправляем на сервер данные */
-                            mServer.sendData("Send text to server".getBytes());
-                        } catch (Exception e) {
-                            Log.e(LOG_TAG, e.getMessage());
-                        }
-                    }
-                }).start();
-            }
-        });
-
-
-        mButtonClose.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-        /* Закрываем соединение */
-                mServer.closeConnection();
-        /* устанавливаем неактивными кнопки отправки и закрытия */
-                mButtonSend.setEnabled(false);
-                mButtonClose.setEnabled(false);
-            }
-        });
-        /////////
 
         //слушатель кнопок
         listener = new View.OnClickListener() {
@@ -146,10 +67,7 @@ public class DoubleGameActivity extends AppCompatActivity {
                         Toast toast = Toast.makeText(getApplicationContext(),
                                 "server!", Toast.LENGTH_SHORT);
                         toast.show();
-                        //поднимаем сервер
-                        Server server = Server.getServer();
-                        Thread t = new Thread(server);
-                        t.start();
+
                         break;
                     case R.id.btn_create_client:
                         Toast toast2 = Toast.makeText(getApplicationContext(),
